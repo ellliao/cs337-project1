7c7bcea43e6b797e777ec4be16aadea145b02ce7
Ellen Liao <ellen.liao.usa@gmail.com>
2024-10-30 10:04:04 -0500

    Update IMDB setup to retrieve only `title.basics`

--- imdb_setup.sh
+++ imdb_setup.sh
@@ -1,4 +1,7 @@
-IMDB_FILES='{name.basics,title.akas,title.basics,title.crew,title.episode,title.principals,title.ratings}'
+## Download and unzip IMDB data.
+# Available datasets: name.basics,title.akas,title.basics,title.crew,title.episode,title.principals,title.ratings
+# Names are easy to identify, so we only want titles
+IMDB_FILES='{title.basics}'
 curl "https://datasets.imdbws.com/$IMDB_FILES.tsv.gz" \
     --create-dirs \
     -o imdb/#1.tsv.gz
